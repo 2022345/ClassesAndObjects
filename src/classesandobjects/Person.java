7297f23e5964649cf5655349e54a828c688589e7
2022345 <2022345@student.cct.ie>
2023-10-04 13:11:28 +0100

    Made a class Person

---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import shapes.Triangle;
+
+/**
+ *
+ * @author suele
+ */
+public class Person {
+    
+    private String name;
+    private int age;
+    double height;
+    boolean isProgrammer = true;
+    Triangle favTriangle;
+    
+}
