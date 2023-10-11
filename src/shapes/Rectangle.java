8de1403f6f8540bfc84543af11b434f6e6d939b7
2022345 <2022345@student.cct.ie>
2023-10-11 15:38:20 +0100

    Update Rectangle.java
    a

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -8,22 +8,39 @@ package shapes;
  *
  * @author suele
  */
-public class Rectangle  extends Shape{
+public class Rectangle  extends Shape implements ShapeD2{
  
    private double height = 5.0;
-   private double widht = 6.2;
+   private double width = 6.2;
+
+   
+   
+     public Rectangle(){
+        
+    }
+    public Rectangle(boolean isFilled, String colour, int height, int width) {
+        super (isFilled, colour);
+        this.height = height;
+        this.width = width;
+    }
     
+        @Override
+        public double calculateAngles (){
+        return 90;
+    }
+   
+  
 
     public double getHeight() {
         return height;
     }
 
     public double getWidht() {
-        return widht;
+        return width;
     }
      public double getArea(){
        
-       return height * widht;
+       return height * width;
        
        
        
